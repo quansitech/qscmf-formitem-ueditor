c49fe2633751702777ab1b3721c33bf40d1266d8
Xhiny <axhiny@163.com>
2024-11-21 18:40:17 +0800

    url_suffix 参数只对上传图片场景有效。

--- src/Lib/Helper.php
+++ src/Lib/Helper.php
@@ -2,21 +2,25 @@
 
 namespace FormItem\Ueditor\Lib;
 
+use FormItem\Ueditor\Lib\Action\Context;
+
 class Helper
 {
 
-    public static function parseUrl($url, $domain = 0, $url_prefix = '', $url_suffix = ''):string{
+    public static function parseUrl($url, $domain = 0, $url_prefix = '', $url_suffix = '', ?array $get_data = []):string{
         $parsed_url = $url;
+        $get_data = $get_data ?: I("get.");
+
 
         if($url_prefix){
             $parsed_url = rtrim($url_prefix, '/') . $parsed_url;
         }
 
-        if($_GET['url_suffix']){
+        if(self::isUploadImage($get_data['action']) && self::isImageFile($url) && $get_data['url_suffix']){
             $parsed_url = $parsed_url . $url_suffix;
         }
 
-        if($_GET['urldomain']){
+        if($get_data['urldomain']){
             $parsed_url = HTTP_PROTOCOL . '://' . SITE_URL . $url;
         }
 
@@ -33,5 +37,27 @@ class Helper
         $res["source"] = htmlspecialchars_decode($source);
 
     }
+
+    public static function isImageFile($file_path): bool
+    {
+        $image_extensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg', 'tiff', 'tif', 'heic', 'cr2', 'nef', 'arw'];
+
+        $extension = strtolower(pathinfo($file_path, PATHINFO_EXTENSION));
+
+        return in_array($extension, $image_extensions, true);
+    }
+
+    public static function isUploadImage($action): bool
+    {
+        $action_list = [
+            Context::ACTION_TYPE_UPLOAD_IMAGE,
+            Context::ACTION_TYPE_UPLOAD_SCRAWL,
+            Context::ACTION_TYPE_WX_CRAWLER,
+            Context::ACTION_TYPE_CATCH_IMAGE
+        ];
+
+        return in_array($action, $action_list, true);
+    }
+
 
 }
