ffb6108e71ee00dff0e5fd58c3f4e88da3865823
jack <958691165@qq.com>
2024-11-22 18:25:07 +0800

    Revert "撤回提交，发新分支"
    This reverts commit 1a19264a5269926286830625bbb7b4eb9643810e.

--- src/FormType/Ueditor/Ueditor.php
+++ src/FormType/Ueditor/Ueditor.php
@@ -1,11 +1,13 @@
 <?php
 namespace FormItem\Ueditor\FormType\Ueditor;
 
+use AntdAdmin\Component\ColumnType\BaseColumn;
 use Illuminate\Support\Str;
+use Qscmf\Builder\Antd\BuilderAdapter\FormAdapter\IAntdFormColumn;
 use Qscmf\Builder\FormType\FormType;
 use Think\View;
 
-class Ueditor implements FormType
+class Ueditor implements FormType, IAntdFormColumn
 {
 
     public function build(array $form_type){
@@ -21,7 +23,14 @@ class Ueditor implements FormType
         }
         $view->assign('server_url', __ROOT__ . '/extends/ueditor/index');
         $content = $view->fetch(__DIR__ . '/ueditor.html');
-
+        
         return $content;
+    }
+
+    public function formColumnAntdRender($options): BaseColumn
+    {
+        $column = new \AntdAdmin\Component\Form\ColumnType\Ueditor($options['name'], $options['title']);
+
+        return $column;
     }
 }
